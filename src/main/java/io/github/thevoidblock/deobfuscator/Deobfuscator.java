fc8f50d978118d5fd749c92caeac7239f0793077
void <wordhh1@gmail.com>
2025-07-19 00:03:59 -0600

    Initial commit

---
+++ src/main/java/io/github/thevoidblock/deobfuscator/Deobfuscator.java
@@ -0,0 +1,10 @@
+package io.github.thevoidblock.deobfuscator;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Deobfuscator implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/thevoidblock/deobfuscator/client/DeobfuscatorClient.java
@@ -0,0 +1,10 @@
+package io.github.thevoidblock.deobfuscator.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeobfuscatorClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
